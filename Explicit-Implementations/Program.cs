657559b269fe6a44af78b892803cefccf1343486
abhishek srivastava <abhi_sri_amity6@yahoo.com>
2017-07-24 11:15:55 +0530

    completed

--- Explicit-Implementations/Program.cs
+++ Explicit-Implementations/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Explicit_Implementations
 {
-    class Program
+
+    interface IA
+    {
+        int math(int x);
+        
+
+    }
+    interface IB
+    {
+        int math(int x);
+
+    }
+
+    class implementer: IA , IB
+    {
+        int IA.math(int x)
+        {
+            return (x+x);
+        }
+        int IB.math(int x)
+        {
+            return x * x;
+
+        }
+    }
+
+    class Program 
     {
         static void Main(string[] args)
         {
+            implementer O = new implementer();
+            IA intA;
+            IB intB;
+            int result;
+
+            intA = O;
+            result = intA.math(7);
+            Console.Write("Output of intA.math(7) :  ");
+            Console.WriteLine(result);
+
+            Console.Read();
+
+
         }
     }
 }
